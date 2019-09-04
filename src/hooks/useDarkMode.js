e55998811fa5e29c6c3ad9d52255e51f64f152b9
Brandon-Harris <brandonharris177@gmail.com>
2019-09-04 17:09:54 -0600

    fixed issue where dark mode was considered opposite

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,12 +2,12 @@ import React, {useState, useEffect} from 'react';
 import useLocalStorage from './useLocalStorage'
 
 const useDarkMode = (darkModeEnabler) => {
-    const [darkMode, setDarkMode] = useLocalStorage( 'dark-mode', darkModeEnabler); 
+    const [darkMode, setDarkMode] = useLocalStorage( 'dark-mode', darkModeEnabler); //this passes the Local storage the key of 'dark-mode' and the itialValue of darkModeEnabler
     useEffect(() => {//this says if dark-mode is already a class remove it, if it is not a class add it
         if (darkMode) {
-            document.body.classList.remove('dark-mode');
-        } else {
             document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
         }
     }, [darkMode])
 
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 
-const useLocalStorage = (key, initialValue) => {
+const useLocalStorage = (key, initialValue) => {//the Itital key is
     const [storedValue, setStoredValue] = useState(() => {
       const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;//if the item exists 
+      return item ? JSON.parse(item) : initialValue;//this returns the item from local storage if it is not there it returns the intialValue.  
     });
 
     const setValue = value => {
